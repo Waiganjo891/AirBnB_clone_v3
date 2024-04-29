0c23dae2b850b598b9a88d50784a2a315aabfd5b
Your Name <you@example.com>
2024-04-29 10:05:48 -0700

    task

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Creates Flask app blueprint
+Creates flask app blueprint
 """
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
-from api.v1.views.state import *
