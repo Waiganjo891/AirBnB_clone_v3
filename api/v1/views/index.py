0680566f7ef46d3ace0882f8115654681779bbe9
Your Name <you@example.com>
2024-04-29 04:58:00 -0700

    Status of your API

---
+++ api/v1/views/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Creates a Flask app; app_views
+"""
+from flask import jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def api_status():
+    """
+    """
+    response = {'status': "OK"}
+    return jsonify(response)
