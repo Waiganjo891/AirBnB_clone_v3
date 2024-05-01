dfe8c4a3df01924c8ec99470c8464b49b2dcebde
Your Name <you@example.com>
2024-05-01 11:57:01 -0700

    Tasks

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ Creates a Flask app; app_views
 """
 from flask import jsonify
 from api.v1.views import app_views
-from models import storage
+from models.storage import count
 
 
 @app_views.route('/status', methods=['GET'])
@@ -16,17 +16,10 @@ def api_status():
     return jsonify(response)
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def get_stats():
     """
     Returns statistics about objects stored in the database
     """
-    stats = {
-        'amenities': storage.count('Amenity'),
-        'cities': storage.count('City'),
-        'places': storage.count('Place'),
-        'reviews': storage.count('Review'),
-        'states': storage.count('State'),
-        'users': storage.count('User'),
-    }
+    stats = count()
     return jsonify(stats)
